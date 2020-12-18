6cef3324f8b4a703f183df606cda4ce89ec960fb
jack zou <36161593+zsms11dys@users.noreply.github.com>
2020-12-18 16:27:14 +0800

    postcontroller中新增根据用户查找动态的接口
    进一步需要数据库实现

--- src/main/java/lookaroundBackend/controller/PostController.java
+++ src/main/java/lookaroundBackend/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -104,13 +105,11 @@ public class PostController {
             Map<String,Object> locationMap = (Map<String,Object>)newRequest.get("location");
             Map<String,Object> publisherMap = (Map<String,Object>)newRequest.get("publisher");
 
-             //need to discuss
             String location = locationMap.get("long").toString()+","+locationMap.get("lat").toString();
             User user = userService.findByUsername(publisherMap.get("userName").toString());
+            Post post = publishService.publishPost(user, newpost.get("text").toString(), location, new HashSet<Byte[]>());
             
-            publishService.publishPost(user, newpost.get("text").toString(), location, null);
-            //end
-
+            data.put("post", new HashMap(getPostMap(post)));
             data.put("msg", "success");
             response = getResonse(200, data);
             return response;
@@ -241,11 +240,8 @@ public class PostController {
                                             @PathVariable(name = "comments",required = false) Integer comments) {
         Map<String,Object> response = new HashMap<String,Object>();
         Map<String,Object> data = new HashMap<String,Object>();
-        Map<String,Object> postMap = new HashMap<String,Object>(); 
         ArrayList<Post> postList = new ArrayList<Post>();
-        //for demo
         ArrayList<Map<String,Object>> allList= new ArrayList<Map<String,Object>>(); 
-        //
         try{
             if(limit == null) limit = 10;
             if(start == null) start = 1;
@@ -339,6 +335,38 @@ public class PostController {
             }
             data.put("downloadCount", allList.size());
             data.put("postList", allList);
+            response = getResonse(200, data);
+            return response;
+        }catch(Exception e){
+            data.clear();
+            data.put("msg", e.getMessage());
+            response = getResonse(300, data);
+            return response;
+        }
+
+    }
+
+    //TODO:新增根据用户查找动态的接口，需要search service提供一下实现
+    //查看动态（按照用户）
+    @RequestMapping(value = "/posts/user/{username}", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> getPostByUser(@PathVariable(name = "username",required = true) String userName) {
+        Map<String,Object> response = new HashMap<String,Object>();
+        Map<String,Object> data = new HashMap<String,Object>();
+        ArrayList<Post> postList = new ArrayList<Post>();
+        ArrayList<Map<String,Object>> allList= new ArrayList<Map<String,Object>>(); 
+        try{
+            User user = userService.findByUsername(userName);
+             //需要实现
+            //postList = searchService.getPostByUser(user);
+            
+            if(postList == null) throw new NullPointerException("no post found");
+            for(Post post:postList){
+                allList.add(new HashMap(getPostMap(post)));
+            }
+            data.put("downloadCount", allList.size());
+            data.put("posts", allList);
+
             response = getResonse(200, data);
             return response;
         }catch(Exception e){
