316447be80e7001e9ae6427824c8c0744fae9c51
RongMingyao <1700012973@pku.edu.cn>
2020-12-28 02:31:04 +0800

    init: 需要编写单元测试和集成测试

---
+++ src/test/java/lookaroundBackend/WebSecurityTest.java
@@ -0,0 +1,15 @@
+package lookaroundBackend;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class WebSecurityTest {
+    
+    @Test
+    public void login(){
+
+    }
+}
